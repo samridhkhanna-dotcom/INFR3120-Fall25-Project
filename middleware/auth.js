f0d502cde85c3fe1b1adccd938a036d24b429942
samridhkhanna-dotcom <samridh.khanna@ontariotechu.net>
2025-11-25 19:14:34 -0500

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,6 @@
 module.exports = (req, res, next) => {
-    // Check if the session exists AND has a logged-in user
     if (req.session && req.session.user) {
         return next();
     }
-
-    // If not logged in → block access
     return res.status(401).json({ message: "Unauthorized" });
 };
