28d0bcf7a9415a7ab5c7d12da2f0f39d3bc4e764
samridhkhanna-dotcom <samridh.khanna@ontariotechu.net>
2025-11-25 19:57:38 -0500

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -4,11 +4,14 @@ const NoteSchema = new mongoose.Schema({
     title: { type: String, required: true },
     course: { type: String, required: true },
     content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
 
+    // Connect note → user
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+    createdAt: { type: Date, default: Date.now }
 },
 {
-    collection:"notes",
-}
-);
+    collection: "notes"
+});
+
 module.exports = mongoose.model("Note", NoteSchema);
