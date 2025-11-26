1cf883421636794d78f63154bf2548f92cc6ae54
Dorothy <116375877+Dorothy-S@users.noreply.github.com>
2025-11-25 22:17:40 -0500

    Note.js

--- models/Note.js
+++ models/Note.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
+// Defins a schema
 const NoteSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    course: { type: String, required: true },
-    content: { type: String, required: true },
+    title: { type: String, required: true }, //Title of note
+    course: { type: String, required: true }, //Course conneted with note
+    content: { type: String, required: true }, //Body of note
 
-    // Connect note → user
+    // Connect note  to user
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now } // makes time of when note was creates
 },
 {
     collection: "notes"
 });
 
-module.exports = mongoose.model("Note", NoteSchema);
+module.exports = mongoose.model("Note", NoteSchema); //exports
