f0a013b2dc262074a485b660135541b4527febef
samridhkhanna-dotcom <samridh.khanna@ontariotechu.net>
2025-11-25 19:17:13 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+{
     username: { type: String, required: true, unique: true },
-    email:    { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-}, { collection: "users" });
+},
+{
+    collection: "users",
+    timestamps: true
+}
+);
 
-// Hash password automatically
+// Hash password before saving
 UserSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-// Compare password with hash
+// Compare entered password with stored hash
 UserSchema.methods.comparePassword = function (password) {
     return bcrypt.compare(password, this.password);
 };
